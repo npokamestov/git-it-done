9ed04ca1ab71eeba8d4dd06680bfcf9886aac7ae
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-09-08 01:01:32 -0400

    added request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,18 @@
-var getUserRepos = function() {
+var getUserRepos = function(user) {
     // console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos");
+    // fetch("https://api.github.com/users/octocat/repos");
+      // format the GitHub api url
+      var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+         // make a request to the url
+        fetch(apiUrl).then(function(response) {
+            response.json().then(function(data){
+            console.log(data);
+        });
+        // console.log("inside", response)
+    });
+    // console.log("outside")
 };
 
 getUserRepos();
